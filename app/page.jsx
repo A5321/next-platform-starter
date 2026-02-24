426a39b5c1ef0cfca1fc1a8b39dd424ca0217805
A5321 <97142976+A5321@users.noreply.github.com>
2026-02-24 12:31:10 +0800

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     border: "1px solid rgba(255,255,255,0.3)",
     textDecoration: "none",
     color: "#ffffff",
-    width: 450,
+    width: 500,
     textAlign: "left",
     fontSize: 14,
   };
