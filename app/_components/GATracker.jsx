c41e0a947911946220740a71193d45b49d491077
A5321 <97142976+A5321@users.noreply.github.com>
2026-03-18 17:40:54 +0800

    Create GATracker.jsx

---
+++ app/_components/GATracker.jsx
@@ -0,0 +1,29 @@
+// app/_components/GATracker.jsx
+"use client";
+
+import Script from "next/script";
+
+export default function GATracker() {
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+  if (!GA_ID) return null;
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', {
+            send_page_view: true
+          });
+        `}
+      </Script>
+    </>
+  );
+}
