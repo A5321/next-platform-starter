652dbe4383cedc4c35bc269f27adb91321bd672a
A5321 <97142976+A5321@users.noreply.github.com>
2026-03-28 22:17:06 +0800

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -28,10 +28,7 @@ export default function Home() {
         alignItems: "flex-start",
         justifyContent: "center",
         padding: "20px 16px",
-        backgroundImage: "
-          linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)),
-          url('/bgr.jpg')
-          ",
+        backgroundImage: "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('/bgr.jpg')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
