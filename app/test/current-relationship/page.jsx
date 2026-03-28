3b07beae0dfd37294607c1c725614213ea1c2d9a
A5321 <97142976+A5321@users.noreply.github.com>
2026-03-28 22:56:18 +0800

    Update page.jsx

--- app/test/current-relationship/page.jsx
+++ app/test/current-relationship/page.jsx
@@ -73,7 +73,7 @@ const pageStyle = {
   display: "flex",
   alignItems: "flex-start",
   justifyContent: "center",
-  padding: "40px 16px",
+  padding: "20px 16px",
   backgroundImage: "url('/bgr.jpg')",
 };
 
