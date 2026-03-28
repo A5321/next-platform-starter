b0b720b7608f09689704697f0f01dde658b65ea2
A5321 <97142976+A5321@users.noreply.github.com>
2026-03-28 23:05:10 +0800

    Update page.jsx

--- app/test/third-person-grey-zone/page.jsx
+++ app/test/third-person-grey-zone/page.jsx
@@ -50,7 +50,10 @@ const pageStyle = {
   alignItems: "flex-start",
   justifyContent: "center",
   padding: "40px 16px",
-  // backgroundColor убираем здесь полностью
+  backgroundImage: "url('/bgr.jpg')",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
 };
 
 const cardStyle = {
