bf3dd4bca0850f72ee585357bc4129f184802b71
A5321 <97142976+A5321@users.noreply.github.com>
2026-03-28 23:07:46 +0800

    Update page.jsx

--- app/test/hyper-controlling-parent/page.jsx
+++ app/test/hyper-controlling-parent/page.jsx
@@ -47,7 +47,7 @@ const pageStyle = {
   display: "flex",
   alignItems: "flex-start",
   justifyContent: "center",
-  padding: "40px 16px",
+  padding: "20px 16px",
   backgroundImage: "url('/bgr.jpg')",
   backgroundSize: "cover",
   backgroundPosition: "center",
