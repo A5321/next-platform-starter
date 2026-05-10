60ef652a7bd869dc9b595d6239a7d0209a71eb5a
A5321 <97142976+A5321@users.noreply.github.com>
2026-05-10 11:52:40 +0800

    Update page.jsx

--- app/test/[slug]/page.jsx
+++ app/test/[slug]/page.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect, useRef } from "react";
 import { getTestBySlug } from "../../lib/testsData";
 import { getProtocolTier } from "../../lib/protocolTiers";
-import EmailCapture from "../../../components/EmailCapture";
+import EmailCapture from "../../components/EmailCapture";
 import ProtocolEmailCapture from "../../../components/ProtocolEmailCapture";
 
 // Static protocol loaders instead of dynamic import
