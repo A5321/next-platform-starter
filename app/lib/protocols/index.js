e1d21a1a910f92ea53a301282984be0439b06967
A5321 <97142976+A5321@users.noreply.github.com>
2026-05-10 16:17:13 +0800

    Update index.js

--- app/lib/protocols/index.js
+++ app/lib/protocols/index.js
@@ -1,7 +1,21 @@
 import { currentRelationshipProtocols } from "./currentRelationship";
 import { youAreAnOptionProtocols } from "./youAreAnOption";
+import { mixedSignalsProtocols } from "./Mixedsignals";
+import { repeatingBreakupProtocols } from "./Repeatingbreakup";
+import { thirdPersonProtocols } from "./Thirdperson";
+import { trustSignalsProtocols } from "./trustSignals";
+import { afterBreachProtocols } from "./afterBreach";
+import { silentExitProtocols } from "./silentExit";
+import { hyperParentProtocols } from "./hyperParent";
 
 export const protocolsByScope = {
   "current-relationship": currentRelationshipProtocols,
   "you-are-an-option": youAreAnOptionProtocols,
+  "mixed-signals": mixedSignalsProtocols,
+  "repeating-breakup": repeatingBreakupProtocols,
+  "third-person-grey-zone": thirdPersonProtocols,
+  "trust-their-signals": trustSignalsProtocols,
+  "after-breach-of-trust": afterBreachProtocols,
+  "silent-exit": silentExitProtocols,
+  "hyper-controlling-parent": hyperParentProtocols,
 };
