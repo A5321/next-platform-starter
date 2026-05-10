fa34defd237dddfab2dafb313dad035f31cd5967
A5321 <97142976+A5321@users.noreply.github.com>
2026-05-10 18:26:29 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -36,9 +36,9 @@ export default function RootLayout({ children }) {
       >
         {children}
         {/* PayPal SDK - KEPT HERE for now (move to payment pages later) */}
-          <Script
-            src={`https://www.paypal.com/sdk/js?client-id=${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}&currency=USD`}
-            strategy="afterInteractive"
+          <Script 
+            src={`https://www.paypal.com/sdk/js?client-id=${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}&currency=USD`} 
+            strategy="lazyOnload" 
           />
       </body>
     </html>
