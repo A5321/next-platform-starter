d65cb5f5e95c605800162685c6125db3ca92f216
A5321 <97142976+A5321@users.noreply.github.com>
2026-05-15 12:16:26 +0800

    Update page.jsx

--- app/test/[slug]/page.jsx
+++ app/test/[slug]/page.jsx
@@ -206,7 +206,7 @@ useEffect(() => {
         return actions.order.create({
           purchase_units: [
             {
-              amount: { value: "1.00", currency_code: "USD" },
+              amount: { value: "15.00", currency_code: "USD" },
               custom_id: `${testSlug}-single`,
               description: currentProtocol.paypalDescription || currentProtocol.productName,
             },
